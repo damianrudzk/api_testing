011c2c2a9eedecdd1e28d84ed7a591e85e0b965b
damianrudzk <32464056+damianrudzk@users.noreply.github.com>
2017-10-16 12:16:48 +0200

    Add files via upload

---
+++ src/test/java/io/coinfirm/Authentication.java
@@ -0,0 +1,124 @@
+package io.coinfirm;
+
+import com.jayway.restassured.http.ContentType;
+import org.junit.*;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+public class Authentication {
+
+    //@Test
+    public void userRegistrationjava() {
+
+        Client client = ClientBuilder.newClient();
+        Entity payload = Entity.json("{  'email': 'damianbeat@gmail.com',  'password': '5h322{R9wp',  'name': 'Joe',  'surname': 'Doe'}");
+        Response response = client.target("https://test.coinfirm.io:81/v2/auth/register")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .post(payload);
+
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers: " + response.getHeaders());
+        System.out.println("body:" + response.readEntity(String.class));
+    }
+
+    //@Test
+    public void userRegistrationRestAssured() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"email\": \"damianbeat@gmail.com\",\n" +
+                        "\"password\": \"Test123!\"\n" +
+                        "\"name\": \"Damian\"\n" +
+                        "\"surname\": \"Tester\"\n" +
+                        "}")
+
+                .when()
+                .post("https://test.coinfirm.io:81/v2/auth/register")
+
+                .then()
+                .statusCode(200);
+
+    }
+
+    //@Test
+    public void retriveAnUserToken() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "   \"email\": \"damian.rudzki@coinfirm.io\",\n" +
+                        "   \"password\": \"Coinfirm09!\"\n" +
+                        "}")
+
+                .when()
+                .post("https://test.coinfirm.io:81/v2/auth/login")
+
+                .then()
+                .body("token", equalTo("GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5"));
+
+    }
+
+    //@Test
+    public void verificationEmail() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+
+
+                .when()
+                .post("https://test.coinfirm.io:81/v2/auth/resend-verification-email")
+
+                .then()
+                .statusCode(200);
+    }
+
+    //@Test
+    public void userActivate() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"key\": \"zPi1A43pQ69OySw/OI2W8Tin9B\"\n" +
+                        "}")
+                .when()
+                .post("https://test.coinfirm.io:81/v2/auth/user-confirm")
+
+                .then()
+                .statusCode(404);
+    }
+
+    //@Test
+    public void userEmailChange() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+
+                .when()
+                .post("https://test.coinfirm.io:81/v2/auth/email-change")
+
+                .then()
+                .statusCode(404);
+    }
+
+
+    @Test
+    public void password_reset() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"email\": \"damian.rudzki@coinfirm.io\"\n" +
+                        "}")
+                .when()
+                .post("http://test.coinfirm.io:81/v2/auth/password-reset")
+
+                .then()
+                .statusCode(204);
+    }
+}
---
+++ src/test/java/io/coinfirm/User.java
@@ -0,0 +1,157 @@
+package io.coinfirm;
+
+import com.jayway.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class User {
+
+    //@Test
+    public void listOfUsers() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+
+
+                .when()
+                .get("https://test.coinfirm.io:81/v2/users")
+
+                .then()
+                .statusCode(200);
+    }
+
+    //@Test
+    public void retriveProfileUser() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+
+
+                .when()
+                .get("https://test.coinfirm.io:81/v2/users/me")
+
+                .then()
+                .statusCode(200);
+    }
+
+    //@Test
+    public void editUserProfile() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"email\": \"damian.rudzki@coinfirm.io\",\n" +
+                        "  \"public\": 1,\n" +
+                        "  \"type\": \"individual\",\n" +
+                        "  \"name\": \"Joe\",\n" +
+                        "  \"surname\": \"Doe\",\n" +
+                        "  \"company_name\": \"Coinfirm\",\n" +
+                        "  \"phone\": \"+48557668900\",\n" +
+                        "  \"website\": \"http://google.pl/\",\n" +
+                        "  \"country\": \"Poland\",\n" +
+                        "  \"city\": \"Warsaw\",\n" +
+                        "  \"street\": \"Burakowska\",\n" +
+                        "  \"post_code\": \"30-015 Warsaw\",\n" +
+                        "  \"tax_id\": \"6982dj0okn381\",\n" +
+                        "  \"company_id\": \"ksoi87jamo02\",\n" +
+                        "  \"personal_id\": \"osd8i82w30\",\n" +
+                        "  \"description\": \"My personal account\",\n" +
+                        "  \"avatar\": \"/upload/avatar1.png\",\n" +
+                        "  \"facebook\": \"http://facebook.com/asd\",\n" +
+                        "  \"twitter\": \"http://twitter.com/asd\",\n" +
+                        "  \"linkedin\": \"http://linkedin.com/asd\"\n" +
+                        "}")
+
+                .when()
+                .patch("https://test.coinfirm.io:81/v2/users/me")
+
+                .then()
+                .statusCode(200);
+    }
+
+    //@Test
+    public void replaceUserProfile() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"email\": \"damian.rudzki@coinfirm.io\",\n" +
+                        "  \"public\": 1,\n" +
+                        "  \"type\": \"individual\",\n" +
+                        "  \"name\": \"Joe\",\n" +
+                        "  \"surname\": \"Doe\",\n" +
+                        "  \"company_name\": \"Coinfirm\",\n" +
+                        "  \"phone\": \"+48557668900\",\n" +
+                        "  \"website\": \"http://google.pl/\",\n" +
+                        "  \"country\": \"Poland\",\n" +
+                        "  \"city\": \"Warsaw\",\n" +
+                        "  \"street\": \"Burakowska\",\n" +
+                        "  \"post_code\": \"30-015 Warsaw\",\n" +
+                        "  \"tax_id\": \"6982dj0okn381\",\n" +
+                        "  \"company_id\": \"ksoi87jamo02\",\n" +
+                        "  \"personal_id\": \"osd8i82w30\",\n" +
+                        "  \"description\": \"My personal account\",\n" +
+                        "  \"avatar\": \"/upload/avatar1.png\",\n" +
+                        "  \"facebook\": \"http://facebook.com/asd\",\n" +
+                        "  \"twitter\": \"http://twitter.com/asd\",\n" +
+                        "  \"linkedin\": \"http://linkedin.com/asd\"\n" +
+                        "}")
+
+                .when()
+                .get("https://test.coinfirm.io:81/v2/users/me")
+
+                .then()
+                .statusCode(200);
+    }
+
+    //@Test
+    public void deleteUserProfile() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer wptbYDnpIC9yw0Wv0EIAOMe0rNUzXQJZJn0FNnuDvipm8wot0yEGFn3yQAaM4u1i").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"email\": \"damian.rudzki@coinfirm.io\",\n" +
+                        "  \"public\": 1,\n" +
+                        "  \"type\": \"individual\",\n" +
+                        "  \"name\": \"Joe\",\n" +
+                        "  \"surname\": \"Doe\",\n" +
+                        "  \"company_name\": \"Coinfirm\",\n" +
+                        "  \"phone\": \"+48557668900\",\n" +
+                        "  \"website\": \"http://google.pl/\",\n" +
+                        "  \"country\": \"Poland\",\n" +
+                        "  \"city\": \"Warsaw\",\n" +
+                        "  \"street\": \"Burakowska\",\n" +
+                        "  \"post_code\": \"30-015 Warsaw\",\n" +
+                        "  \"tax_id\": \"6982dj0okn381\",\n" +
+                        "  \"company_id\": \"ksoi87jamo02\",\n" +
+                        "  \"personal_id\": \"osd8i82w30\",\n" +
+                        "  \"description\": \"My personal account\",\n" +
+                        "  \"avatar\": \"/upload/avatar1.png\",\n" +
+                        "  \"facebook\": \"http://facebook.com/asd\",\n" +
+                        "  \"twitter\": \"http://twitter.com/asd\",\n" +
+                        "  \"linkedin\": \"http://linkedin.com/asd\"\n" +
+                        "}")
+                .when()
+                .delete("https://test.coinfirm.io:81/v2/users/me")
+
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void changeUserProfilePassword() {
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer GHxLfIGzCmaOzO1FYZAP7INZ8NnfBnzA92W69rl570rUNh1uKvOYwkX4a2Otf7d5").and().header("Content-Type", "application/json")
+                .body("{\n" +
+                        "  \"current_password\": \"Coinfirm09!\",\n" +
+                        "  \"password\": \"Coinfirm08!\"\n" +
+                        "}")
+                .when()
+                .put("https://test.coinfirm.io:81/v2/users/me/password")
+
+                .then()
+                .statusCode(200);
+    }
+}
